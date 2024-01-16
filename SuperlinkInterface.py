e51146d58777e70e5a3b43f02acae3f918f309d6
samy kamkar <git@samy.pl>
2024-01-15 17:51:12 -0800

    autodetect serial interface + text fixes + debug

--- SuperlinkInterface.py
+++ SuperlinkInterface.py
@@ -8,6 +8,8 @@ provides:
 - Switching between Auto + Manual shutdown modes
 """
 
+debug = 0
+
 # Offset and gain values for temperature variables
 Tcold_offset = 5.814
 Tcold_gain = -0.01559
@@ -22,11 +24,18 @@ SuperlinkModeOptions = ["Auto", "Manual"]
 SuperlinkStateOptions = ["Initial", "Cooldown", "Regulate", "Bypass", "Shutdown"]
 
 from tkinter import *
+import glob
 import time
 import serial
 import signal
+import sys
+import serial.tools.list_ports
 
-def truncate(f, n): # !hanks internet stranger! http://stackoverflow.com/questions/783897/truncating-floats-in-python
+def p(msg):
+	if debug:
+		print(msg)
+
+def truncate(f, n): # Thanks internet stranger! http://stackoverflow.com/questions/783897/truncating-floats-in-python
 	'''Truncates/pads a float f to n decimal places without rounding'''
 	s = '{}'.format(f)
 	if 'e' in s or 'E' in s:
@@ -34,20 +43,31 @@ def truncate(f, n): # !hanks internet stranger! http://stackoverflow.com/questio
 	i, p, d = s.partition('.')
 	return '.'.join([i, (d+'0'*n)[:n]])
 
+# try to find the correct serial port
+def findPort():
+	for port in list(serial.tools.list_ports.comports()):
+		p(vars(port))
+		if port.hwid.find('SER=') != -1:
+			return(port.device)
+	return ""
+
 def SerialQuery(query):
-	print("TX> " + query)
+	p("TX> " + query)
 	query_bytes = query.encode('ascii') # convert ascii input to bytes for serial
 	ser.write(query_bytes)
 	try:
 		response = ser.readline()
-		print("rx  " + response)
+		resp = response.decode('ascii').strip()
+		p("rx  " + resp)
+		if len(resp) > 0:
+			p("")
 	except serial.serialutil.SerialException:
 		# if no response (ie if serial cable pulled out), return nothing. This doesn't acutally seem to work, need to fix.
 		response = b''
 	return response.decode('ascii').strip()
 
 
-def ConvertTemp(raw_T, startpoint, endpoint, offset, gain):
+def convertTemp(raw_T, startpoint, endpoint, offset, gain):
 	# need to convert the responses from bytes to ascii to integer
 	# characters 22 to 25 are the cold side wide range temperature reading raw value. Need to convert it to Kelvin.
 
@@ -61,8 +81,8 @@ def ConvertTemp(raw_T, startpoint, endpoint, offset, gain):
 		print("Error converting temperature!")
 		return(0.00)
 
-def ConvertPower(raw_P, startpoint, endpoint, scaling):
-	# Convert power response from bytes to ascii to integer
+def convertPower(raw_P, startpoint, endpoint, scaling):
+	# convert power response from bytes to ascii to integer
 	try:
 		pwr = int(raw_P[startpoint:endpoint], 16)
 
@@ -75,11 +95,11 @@ def ConvertPower(raw_P, startpoint, endpoint, scaling):
 		return(0.00)
 
 
-# returns the position of the start of the nth space (spacenum) after the SearchFor string.
-def FindSpaceAfter(SearchMsg, SearchFor, SpaceNum):
+# returns the position of the start of the nth char after the SearchFor string
+def findAfter(char, SearchMsg, SearchFor, SpaceNum):
 	StartPt = SearchMsg.find(SearchFor) + len(SearchFor)-1
 	for x in range(1, SpaceNum+1):
-		StartPt = SearchMsg.find(" ", StartPt+1)
+		StartPt = SearchMsg.find(char, StartPt+1)
 
 	return StartPt
 
@@ -132,9 +152,10 @@ class App(Frame):
 		Button(self, text='Quit', command=self.quit).grid(row=ButtonsRow, column = 3)
 
 
-
 		ComEntry = Entry(self, textvariable=self.ComPortText)
 		ComEntry.grid(row = 4, column = 1)
+		ComEntry.insert(0, findPort())
+
 		Label(self, text = "COM port").grid(row=4, column = 0)
 
 		Label(self, text = "Time elapsed").grid(row=0, column = 2)
@@ -170,32 +191,32 @@ class App(Frame):
 		self._elapsedtime = time.time() - self._start
 		self._setTime(self._elapsedtime)
 		if ser.isOpen():
-			# !irst, query for the temperatures
-			query = '<TP OP="GT" LC="MS"/>' # !nquiry command for cold side and rejection temps.
-			# !old side temp is second bit (between spaces 1 and 2)
-			# !ejection temp is third bit (between spaces 2 and 3)
+			# First, query for the temperatures
+			query = '<TP OP="GT" LC="MS"/>' # Inquiry command for cold side and rejection temps.
+			# Cold side temp is second bit (between spaces 1 and 2)
+			# Rejection temp is third bit (between spaces 2 and 3)
 			queryRet = SerialQuery(query)
 			if len(queryRet)==0:
 				ColdTemp = "Serial timed out"
 				RejTemp = "Serial timed out"
 			else:
-				ColdTemp = ConvertTemp(queryRet, FindSpaceAfter(queryRet, query, 1) + 1, FindSpaceAfter(queryRet, query, 2), Tcold_offset, Tcold_gain)
+				ColdTemp = convertTemp(queryRet, findAfter(" ", queryRet, query, 1) + 1, findAfter(" ", queryRet, query, 2), Tcold_offset, Tcold_gain)
 				ColdTemp = round(ColdTemp, 1)
-				RejTemp = ConvertTemp(queryRet, FindSpaceAfter(queryRet, query, 2) + 1, FindSpaceAfter(queryRet, query, 3), Trej_offset, Trej_gain)
+				RejTemp = convertTemp(queryRet, findAfter(" ", queryRet, query, 2) + 1, findAfter(" ", queryRet, query, 3), Trej_offset, Trej_gain)
 				RejTemp =  round(RejTemp,1)
 
-			# !ext, query for the power
-			query = '<PW OP="GT" LC="MS"/>' # !nquiry command for cooler power
+			# Text, query for the power
+			query = '<PW OP="GT" LC="MS"/>' # inquiry command for cooler power
 			queryRet = SerialQuery(query)
 
 			if len(queryRet)==0:
 				Power = "Serial timed out"
 			else:
-				# !ower is the first (zeroth) item in this query. The FindSpaceAfter should still work fine.
-				Power = ConvertPower(queryRet, FindSpaceAfter(queryRet, query, 0) + 1, FindSpaceAfter(queryRet, query, 1), Power_Scaling)
+				# power is the first (zeroth) item in this query. The findAfter should still work fine.
+				Power = convertPower(queryRet, findAfter(" ", queryRet, query, 0) + 1, findAfter(" ", queryRet, query, 1), Power_Scaling)
 				Power = round(Power, 1)
 
-			# !ast, query for the Superlink status and auto/manual mode
+			# last, query for the Superlink status and auto/manual mode
 
 			query = '<TP OP="GT" LC="SM"/>'
 			queryRet = SerialQuery(query)
@@ -205,12 +226,12 @@ class App(Frame):
 				SuperlinkState= "Serial timed out"
 			else:
 
-				SuperlinkMode= int(queryRet[FindSpaceAfter(queryRet, query, 0)+1:FindSpaceAfter(queryRet, query, 0)+2])
+				SuperlinkMode= int(queryRet[findAfter(" ", queryRet, query, 0)+1:findAfter(" ", queryRet, query, 0)+2])
 				SuperlinkMode = SuperlinkModeOptions[SuperlinkMode]
-				SuperlinkState= int(queryRet[FindSpaceAfter(queryRet, query, 1)+1:FindSpaceAfter(queryRet, query, 1)+2])
+				SuperlinkState= int(queryRet[findAfter(" ", queryRet, query, 1)+1:findAfter(" ", queryRet, query, 1)+2])
 				SuperlinkState= SuperlinkStateOptions[SuperlinkState]
 
-			# !rite everything to proper labels
+			# write everything to proper labels
 			self.TcoldWindow.set(ColdTemp)
 			self.TrejWindow.set(RejTemp)
 			self.PowerWindow.set(Power)
@@ -218,7 +239,7 @@ class App(Frame):
 			self.SuperlinkStateWindow.set(SuperlinkState)
 
 		else:
-			# !f serial is off, do nothing
+			# if serial is off, do nothing
 			self.TcoldWindow.set("Serial connection off")
 
 
@@ -239,7 +260,7 @@ class App(Frame):
 				self.TcoldWindow.set("Need to enter a COM port")
 			else:
 				try:
-					# !tart serial connection on the proper port.
+					# start serial connection on the proper port.
 					ser = serial.Serial(port=self.ComPortText.get(), baudrate=19200, timeout=3)
 				except serial.serialutil.SerialException:
 
@@ -260,11 +281,11 @@ class App(Frame):
 			self._elapsedtime = time.time() - self._start
 			self._setTime(self._elapsedtime)
 			self._running = 0
-			# !lose serial port
+			# close serial port
 			ser.close()
 
 	def PowerToggle(self):
-		# !oggles the Cryocooler between auto and manual modes
+		# toggles the Cryocooler between auto and manual modes
 		global ser
 		if self._running and ser.isOpen():
 			if self.SuperlinkModeWindow.get() == "Auto":
@@ -296,3 +317,45 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+"""
+  {
+    "device": "/dev/cu.Maker3-3BBD",
+    "name": "cu.Maker3-3BBD",
+    "description": "n/a",
+    "hwid": "n/a",
+    "vid": "None",
+    "pid": "None",
+    "serial_number": "None",
+    "location": "None",
+    "manufacturer": "None",
+    "product": "None",
+    "interface": "None"
+  },
+  {
+    "device": "/dev/cu.usbmodem24303",
+    "name": "cu.usbmodem24303",
+    "description": "EVOCam2",
+    "hwid": "USB VID:PID=2F88:0001 LOCATION=0-2.4.3",
+    "vid": 12168,
+    "pid": 1,
+    "serial_number": "None",
+    "location": "0-2.4.3",
+    "manufacturer": "VisionEngineering",
+    "product": "EVOCam2",
+    "interface": "None"
+  },
+  {
+    "device": "/dev/cu.usbserial-FTH145P2",
+    "name": "cu.usbserial-FTH145P2",
+    "description": "TTL232R-3V3",
+    "hwid": "USB VID:PID=0403:6001 SER=FTH145P2 LOCATION=1-1",
+    "vid": 1027,
+    "pid": 24577,
+    "serial_number": "FTH145P2",
+    "location": "1-1",
+    "manufacturer": "FTDI",
+    "product": "TTL232R-3V3",
+    "interface": "None"
+  }
+"""
